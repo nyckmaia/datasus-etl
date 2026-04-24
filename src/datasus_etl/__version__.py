dff6af3c8f9ea8bbe02219aa8a395b7049089740
Nycholas Maia <nyckmaia@gmail.com>
2026-04-24 14:44:30 -0300

    chore(release): bump version to 0.1.8 and update build settings

--- src/datasus_etl/__version__.py
+++ src/datasus_etl/__version__.py
@@ -4,5 +4,5 @@ This file is regenerated from the repository-root VERSION file by
 the hatch build hook. Edit VERSION, not this file.
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.7"
 __version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
